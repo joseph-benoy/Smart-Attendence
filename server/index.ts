307f90866da167fe4bda478352428aa475d988a8
joseph-benoy <josephbenoy03@gmail.com>
2022-03-22 00:01:59 +0530

    fixed nodemon and ts-node configuration

---
+++ server/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import 'dotenv/config';
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.clear();
+    console.log("Server started at port : "+process.env.PORT);
+})
