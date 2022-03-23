156d4dd542c2f677c488df9e65009e078f32604c
joseph-benoy <josephbenoy03@gmail.com>
2022-03-23 08:47:52 +0530

    added tabs

--- ui/src/pages/Admin/index.tsx
+++ ui/src/pages/Admin/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import HeaderPlain from '../../layout/HeaderPlain';
-
+import { Tabs,Tab } from 'react-bootstrap';
 export interface IAdminProps {
 }
 
@@ -8,6 +8,18 @@ export default function Admin (props: IAdminProps) {
   return (
     <>
     <HeaderPlain/>
+    <main>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+        <Tab eventKey="dept" title="Departments">
+        </Tab>
+        <Tab eventKey="courses" title="Courses">
+        </Tab>
+        <Tab eventKey="teachers" title="Teachers">
+        </Tab>
+        <Tab eventKey="student" title="Student">
+        </Tab>
+      </Tabs>
+    </main>
     </>
   );
 }
